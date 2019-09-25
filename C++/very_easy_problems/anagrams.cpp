4db95c8f19bd7213a5803d96f8c43943da6d3ad7
SHUBHAM THAKUR <shubhamwert@gmail.com>
2019-09-25 15:51:42 +0530

    prime number\nfirst commit

---
+++ C++/very_easy_problems/anagrams.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<cmath>
+#include<cstring>
+#include <bits/stdc++.h> 
+
+using namespace std;
+int main(){
+    string s1,s2;
+    cin>>s1;
+    cin>>s2;
+    sort(s1.begin(),s1.end());
+    sort(s2.begin(),s2.end());
+    int n;
+    if(s1.length()<s2.length()){
+        n=s1.length();
+    }
+    else
+    {
+        n=s2.length();
+    }
+    for(int i=0;i<n)
+
+
+ }
