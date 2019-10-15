ed9cc997a185ef7745568bd6923e003a297f227e
SHUBHAM THAKUR <shubhamwert@gmail.com>
2019-10-15 20:28:59 +0530

    kick started

---
+++ KS/NumberGuessing.py
@@ -0,0 +1,41 @@
+import sys
+def T():
+    t=int(input())
+    while t>0:
+        sys.stdout.flush()
+
+        a, b = [int(x) for x in input().split()]
+        a,b=a+1,b+1
+        N=int(input())
+        for i in range(0,N+1):
+           print(int((a+b)//2),"\n")
+           if(a==b):
+                    print("WRONG_ANSWER")
+                    sys.exit()
+           sys.stdout.flush()
+           s=input().strip()
+           if(s=="CORRECT"):
+               break
+           else: 
+               if(s=="TOO_SMALL"):
+                   
+                   a=int((a+b)//2)
+                  
+
+               else:
+                   if(s=="TOO_LARGE"):
+                       
+                       b=int((a+b)//2)
+                       
+
+                   else:
+                       if(s=="WRONG_ANSWER"):
+                           sys.exit()
+                       else:
+                           print("WRONG_INPUT")
+                           print("a = ",a,"\nb = ",b)
+                           sys.exit()
+
+        t=t-1
+
+T()
