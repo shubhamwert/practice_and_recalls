80ebccc16d632573be5a36aedc481d45fc9f4281
shubham <shubhamwert@gmail.com>
2020-10-20 11:56:07 +0530

    additions

---
+++ C++/codeforces/900/sage_birthday.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<list>
+
+using namespace std;
+
+
+
+int main(){
+    int n;
+    cin>>n;
+    int r=n-1;
+    int l=0;
+    list<int> v;
+    int a[n];
+    int counter=0;
+    for(int i=0;i<n;i++){cin>>a[i];}
+    v.push_back(a[n-1]);
+    while(l<r){
+            if(!(counter%2==0)){
+                r--;
+                v.push_back(a[r]);
+                r--;
+                v.push_front(a[r]);
+            }
+            else{
+                v.push_front(a[l]);
+                l++;
+                v.push_back(a[l]);
+                l++;
+            }
+            counter++;
+
+
+    }
+    cout<<counter<<endl;
+    for (auto it = v.begin(); it != v.end(); ++it) 
+        cout <<*it<<" "; 
+    return 0;
+
+}
